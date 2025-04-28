0c05e5b3fc20e635bd501e5c0dd71606d5dbfbea
Sergii Sergiienko <serjungle@gmail.com>
2025-04-27 20:28:43 -0400

    Reconnected project after cleaning folders

---
+++ test3.js
@@ -0,0 +1,15 @@
+//I have access to repo 
+
+function greeting(name){
+  return "Hello, " + name;
+}
+
+console.log(greeting('Sergii'));
+
+//this is such funny game
+
+function greeting(name){
+  return "Nice to meet u, " + name;
+}
+
+console.log(greeting('Faviola'));
